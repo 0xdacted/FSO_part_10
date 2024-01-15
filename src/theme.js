22f106a9c8ab914fb0207488f2b1db976b20fd03
Red <crabmarket56@gmail.com>
2024-01-15 14:01:16 -0500

    Make font platform specific

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
@@ -9,7 +11,7 @@ const theme = {
     subheading: 16,
   },
   fonts: {
-    main: 'System',
+    main: Platform.OS === 'ios' ? 'Arial' : 'Roboto',
   },
   fontWeights: {
     normal: '400',
