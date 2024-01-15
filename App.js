abad37694018cfb9d4b6ec0f54fbcf79399d3203
Red <crabmarket56@gmail.com>
2024-01-15 18:39:18 -0500

    Add env variables to config and implement

--- App.js
+++ App.js
@@ -4,9 +4,11 @@ import { ApolloProvider } from '@apollo/client';
 import createApolloClient from './src/utils/apolloClient';
 import Main from './src/components/Main';
 
+
 const apolloClient = createApolloClient();
 
 const App = () => {
+
   return (
     <>
       <NativeRouter>
