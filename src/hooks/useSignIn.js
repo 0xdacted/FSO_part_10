5b17df86bd4d5aed373bd81598dcaf8c6d75ac27
Red <crabmarket56@gmail.com>
2024-01-16 14:02:06 -0500

    Store access token in local storage

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,17 +1,23 @@
 import { useMutation } from '@apollo/client';
 import { SIGN_IN_MUTATION } from '../graphql/mutations';
 import useAuthStorage from '../hooks/useAuthStorage';
+import { useApolloClient } from '@apollo/client';
 
 
 const useSignIn = () => {
   const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
 
   const [mutate, result] = useMutation(SIGN_IN_MUTATION);
 
   const signIn = async ({ username, password }) => {
-    return await mutate({ 
+    const data =  await mutate({ 
       variables: { username, password } 
     });
+    console.log(data);
+    await authStorage.setAccessToken(data.data.authenticate.accessToken);
+    apolloClient.resetStore();
+    return data
   };
 
   return [signIn, result];
