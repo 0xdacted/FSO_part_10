9319dbc9c3575f91571d76ec89aa986c42880ee0
Red <crabmarket56@gmail.com>
2024-01-16 13:28:20 -0500

    Add navigation back to main

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-native';
 import { Formik } from 'formik';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import * as Yup from 'yup';
@@ -31,12 +32,15 @@ const validationSchema = Yup.object({
 
 const SignIn = () => {
     const [signIn] = useSignIn(); 
+    const navigate = useNavigate();
+
 
     const onSubmit = async (values) => {
         const { username, password } = values;
 
         try {
             const { data } = await signIn({ username, password });
+            navigate('/')
             console.log(data);
 
         } catch (e) {
