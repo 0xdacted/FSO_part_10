7f1aea9b64261a24794dc36ecd04aaa178b4e75b
Red <crabmarket56@gmail.com>
2024-01-16 13:18:21 -0500

    Implement authStorage into apolloClient

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
 import createApolloClient from './src/utils/apolloClient';
 import Main from './src/components/Main';
+import AuthStorage from './src/utils/authStorage';
 
-
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
 
