859d8196626ff9eecb9cd338e80523eb573ce0f0
Red <crabmarket56@gmail.com>
2024-01-16 13:21:12 -0500

    Add authStorageContext

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,7 +1,12 @@
 import { useMutation } from '@apollo/client';
 import { SIGN_IN_MUTATION } from '../graphql/mutations';
+import { useContext } from 'react';
+import AuthStorageContext from '../contexts/AuthStorageContext';
+
 
 const useSignIn = () => {
+  const authStorage = useContext(AuthStorageContext);
+
   const [mutate, result] = useMutation(SIGN_IN_MUTATION);
 
   const signIn = async ({ username, password }) => {
